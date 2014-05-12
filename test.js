1b538db46388bb67918163516d78ae085e100f07
Isaac Cohen <icohen@leapmotion.com>
2014-05-11 20:13:21 -0700

    first working version of wombs-womb. YAY

--- test.js
+++ test.js
@@ -1,13 +1,76 @@
+/*
+
+   Welcome to the incubator.
+
+   TODO:
+
+
+    - Something screwy is going on you add the fft 
+      to the analyser. an off by x/2 because of 
+      frequencyBinCount vs this.analyser.array.
+
+
+*/
+
+var womb = require( 'wombs-womb' );
 
 var audioController = require( 'wombs-audio-controller' );
-audioController._start();
+var UserAudio       = require( 'wombs-audio-user-audio' );
 
-update();
+var THREE = require('three');
 
-function update(){
+var renderer , camera , scene;
 
-  audioController._update();
-  window.requestAnimationFrame( update );
+var userAudio;
+
+
+
+//init();
+
+console.log( womb );
+womb.addToStartArray( function(){
+
+
+  var self = this;
+  var update = function(){
+
+    self._update();
+    window.requestAnimationFrame( update );
+
+
+  }
+  
+  update();
+
+
+});
+
+function init(){
+
+  userAudio = new UserAudio( audioController );
+
+  womb.addComponent( userAudio );
+
+  audioController.texture.createDebugMesh( womb._three );
+
+
+
+  audioController.mute();
+
+  womb._start();
+
+  womb.audioController.mute();
+  
 
 }
+
+init();
+/*function update(){
+
+  audioController._update();
+
+  renderer.render( scene , camera );
+  window.requestAnimationFrame( update );
+
+}*/
 
